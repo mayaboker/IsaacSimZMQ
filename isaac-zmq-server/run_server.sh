23bb014a2467d003cf54973db81ca55d27d11735
user <user@lion>
2025-12-02 14:43:40 +0200

    fix: Improve NVIDIA GPU and X11 support in server container

--- isaac-zmq-server/run_server.sh
+++ isaac-zmq-server/run_server.sh
@@ -1,14 +1,11 @@
-xhost +local:appuser
+xhost +local:
 docker run --gpus all --network host \
        -e DISPLAY=$DISPLAY \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -e NVIDIA_DRIVER_CAPABILITIES=all \
+       -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
        -e XAUTHORITY=$XAUTHORITY \
        -v $XAUTHORITY:$XAUTHORITY \
        -v ./src:/isaac-zmq-server/src \
-       --device /dev/input \
-       --device /dev/input/event21 \
-       --device /dev/input/event22 \
-       --device /dev/input/event23 \
        --privileged \
        -it --rm \
         isaac-zmq-server bash
